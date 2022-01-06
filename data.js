e4c4b6d7fca1dde77be5451cf53ad3e08591e5af
Drew Pate <andrewcpate@gmail.com>
2022-01-06 14:00:12 -0500

    create data.js file, add complete CRUD operations to server.js

---
+++ data.js
@@ -0,0 +1,22 @@
+let data = [
+  {
+    id: 1,
+    title: "New-ish Tshirt",
+    description: "lightly used tshirt, stained with red",
+    price: 20,
+  },
+  {
+    id: 2,
+    title: "Bottle",
+    description: "sealed bottle of water",
+    price: 50,
+  },
+  {
+    id: 3,
+    title: "Monocycle",
+    description: "like a bike, but one wheel and no handlebar",
+    price: 600,
+  },
+];
+
+module.exports = data;
--- server.js
+++ server.js
@@ -1,18 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 5000;
+let data = require("./data");
+app.use(express.json());
 
-app.get('/', (req, res) => {
-
-    res.send('Hello from route /')
+app.get("/items", (req, res) => {
+  res.send(data);
 });
 
-app.post('/', (req, res) => {
-    res.send('POST request to route /')
+app.get("/items/:id", (req, res) => {
+  const foundItem = data.find((item) => item.id === +req.params.id);
+  res.send(foundItem);
 });
 
+app.put("/items/:id", (req, res) => {
+  const newArray = data.map((item) => {
+    if (item.id === +req.params.id) {
+      return { id: item.id, ...req.body };
+    } else {
+      return item;
+    }
+  });
+  data = newArray;
+  res.send(req.body);
+});
 
+app.delete("/items/:id", (req, res) => {
+  const filteredArray = data.filter((item) => item.id !== +req.params.id);
+  data = filteredArray;
+  res.sendStatus(204);
+});
+
+app.post("/", (req, res) => {
+  res.send("POST request to route /");
+});
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
-})
+  console.log(`Running on port ${PORT}`);
+});
